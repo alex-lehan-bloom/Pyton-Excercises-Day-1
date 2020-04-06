714f188f76fe7465834329888fd67357d617e835
Alex Bloom <lehanbloom@gmail.com>
2020-04-06 16:28:42 +0300

    Day 2: Completed excercise 4 - name checker using a generator.

--- Day 2/name_checker.py
+++ Day 2/name_checker.py
@@ -1,18 +1,11 @@
 
-
 def name_checker(list_of_names):
     for name in list_of_names:
-        if name.isspace() == True:
+        if ' ' not in name:
             yield name
-
-    # peeps_missing_last_name = (name if name.isspace() == True else "nothing" for name in list_of_names)
-    # print(list_of_names)
-    # for name in peeps_missing_last_name:
-    #     print(name)
 
 
 list_of_names = ["Alex Bloom", "Yaakov", "Emma", "Ruthy Lewis", "David"]
 people_missing_last_name = name_checker(list_of_names)
-print(people_missing_last_name)
 for person in people_missing_last_name:
     print(person)
